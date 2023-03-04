78be36933a58f0d1d6688fe5d66d2d063b39a548
SherlockAlex <101167329+SherlockAlex@users.noreply.github.com>
2023-03-04 23:12:24 +0800

    添加重力

--- Physics.cpp
+++ Physics.cpp
@@ -5,12 +5,11 @@ float startTime = 0;
 
 void ApplyGrivaty(float* y, bool on)
 {
-	startTime = SDL_GetTicks();
-	/*在on的情况下,完成物理重力的模拟*/
-	if (!on) {
-		return;
-	}
-
 	
+	//还要有一个设置掉落时间的，进来前开始掐秒表
+	float value = *y;
+	startTime = on ? (startTime + 3) : 0;
+	value += 0.0025 * startTime * startTime;
+	*y = value;
 
 }
